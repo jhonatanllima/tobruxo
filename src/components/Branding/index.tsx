040b1ceab83f38f655f240a463eb65122633cef6
Jhonatan Lima <jhonatan.vinicius.lima@gmail.com>
2021-03-25 22:11:09 -0300

    Finalized initial Styling on Branding component

--- src/components/Branding/index.tsx
+++ src/components/Branding/index.tsx
@@ -1,4 +1,5 @@
 import { FaRegHandshake } from 'react-icons/fa';
+import { AiOutlineClockCircle } from 'react-icons/ai'
 
 import * as S from './styles'
 
@@ -9,7 +10,68 @@ export function Branding() {
         <img src="images/mao.jpg" alt="maozinha"/>
       </header>
 
-      <section />
+      <aside >
+        <section>
+          <img src="https://flexiblog-agency.netlify.app/static/90a14b9d2337e448de6a033b1c83c93b/c4b86/image.webp" alt="teste"/>
+
+          <main>
+            <h3>Advertising</h3>
+
+            <h2>Markdown Language Sample Blog</h2>
+
+            <footer>
+              <span>Jane Smith</span>
+              <p>may 15, 2021  <AiOutlineClockCircle  style={{marginBottom: -2, color: '#718096'}}/> 2 min</p>
+            </footer>
+          </main>
+        </section>
+
+        <section>
+          <img src="https://flexiblog-agency.netlify.app/static/90a14b9d2337e448de6a033b1c83c93b/c4b86/image.webp" alt="teste"/>
+
+          <main>
+            <h3>Advertising</h3>
+
+            <h2>Markdown Language Sample Blog</h2>
+
+            <footer>
+              <span>Jane Smith</span>
+              <p>may 15, 2021  <AiOutlineClockCircle  style={{marginBottom: -2, color: '#718096'}}/> 2 min</p>
+            </footer>
+          </main>
+        </section>
+
+        <section>
+          <img src="https://flexiblog-agency.netlify.app/static/90a14b9d2337e448de6a033b1c83c93b/c4b86/image.webp" alt="teste"/>
+
+          <main>
+            <h3>Advertising</h3>
+
+            <h2>Markdown Language Sample Blog</h2>
+
+            <footer>
+              <span>Jane Smith</span>
+              <p>may 15, 2021  <AiOutlineClockCircle  style={{marginBottom: -2, color: '#718096'}}/> 2 min</p>
+            </footer>
+          </main>
+        </section>
+
+        <section>
+          <img src="https://flexiblog-agency.netlify.app/static/90a14b9d2337e448de6a033b1c83c93b/c4b86/image.webp" alt="teste"/>
+
+          <main>
+            <h3>Advertising</h3>
+
+            <h2>Markdown Language Sample Blog</h2>
+
+            <footer>
+              <span>Jane Smith</span>
+              <p>may 15, 2021  <AiOutlineClockCircle  style={{marginBottom: -2, color: '#718096'}}/> 2 min</p>
+            </footer>
+          </main>
+        </section>
+      </aside>
+
 
       <div >
         <FaRegHandshake size={70} color={'#9F7AEA'} />
--- src/components/Branding/styles.ts
+++ src/components/Branding/styles.ts
@@ -26,12 +26,111 @@ export const Container = styled.div`
     }
   }
 
-  section{
+  aside{
     min-height: 32rem;
     border-radius: 18px;
 
     grid-area: section;
-    background: var(--background-card);
+
+
+    display: flex;
+    flex-wrap: wrap;
+    /* justify-content: center; */
+
+    section{
+      position: relative;
+
+      width: 49%;
+      height: 48%;
+      padding: 0.8rem 1rem;
+      border-radius: 18px;
+      border-left: 6px solid var(--blue-dark);
+
+      display: flex;
+      align-items: center;
+      
+      transition: .2s;
+      background: var(--background-card);
+
+      :hover{
+        cursor: pointer;
+        transform: translateY(-0.35rem);
+      }
+      
+      img{
+        width: 35%;
+        height: 100%;
+        border-radius: 8px;
+
+        object-fit: cover;
+      }
+
+      main{
+        position: relative;
+
+        width: 100%;
+        height: 100%;
+        padding-left: 1.8rem;
+
+        h3{
+          width: 10rem;
+          height: 3rem;
+          margin-top: 0.8rem;
+          border-radius: 12px;
+          padding: 0.6rem 1.6rem ;
+
+          font-size: 1.3rem;
+          background: var(--blue);
+        }
+
+        h2{
+          margin-top: 1.3rem;
+
+          font-size: 1.8rem;
+        }
+
+        footer{
+          bottom: 0;
+          position: absolute;
+
+          display: flex;
+          align-items: center;
+          justify-content: center;
+
+          span{
+            margin-right: 1rem;
+            font-weight: 400;
+          }
+
+          p{
+            margin-top: 0.25rem;
+            font-size: 1.2rem;
+            font-weight: lighter;
+          }
+        }
+      }
+
+
+      :first-child{
+        margin-right: 1.1%;
+        margin-bottom: 1%
+      }
+
+      :nth-child(2){
+        margin-left: 0.5%;
+        margin-bottom: 1%
+      }
+
+      :nth-child(3){
+        margin-right: 1.1%;
+        margin-top: 1%
+      }
+
+      :nth-child(4){
+        margin-left: 0.5%;
+        margin-top: 1%
+      }
+    }
   }
 
   div{
