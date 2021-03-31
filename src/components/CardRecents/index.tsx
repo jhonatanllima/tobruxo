2abf992db7d1bc3bb2a7470c5840a1dc932ac87f
Jhonatan Lima <jhonatan.vinicius.lima@gmail.com>
2021-03-30 23:26:52 -0300

    Finalized estrure of CardRecents

--- src/components/CardRecents/index.tsx
+++ src/components/CardRecents/index.tsx
@@ -3,12 +3,41 @@ import * as S from './styles';
 export function CardRecents() {
   return (
     <S.Container>
-      <h3>Conheça os Novos Lançamentos</h3>
-      <div>
-        <strong>Gabriel Ribeiro</strong>
-        <p>1 min</p>
-        <p>15/04/21</p>
-      </div>
+      <article>
+        <h3>Conheça os Novos Lançamentos</h3>
+        <div>
+          <strong>Gabriel Ribeiro</strong>
+          <p>1 min</p>
+          <p>15/04/21</p>
+        </div>
+      </article>
+
+      <article>
+        <h3>Conheça os Novos Lançamentos</h3>
+        <div>
+          <strong>Gabriel Ribeiro</strong>
+          <p>1 min</p>
+          <p>15/04/21</p>
+        </div>
+      </article>
+
+      <article>
+        <h3>Conheça os Novos Lançamentos</h3>
+        <div>
+          <strong>Gabriel Ribeiro</strong>
+          <p>1 min</p>
+          <p>15/04/21</p>
+        </div>
+      </article>
+
+      <article>
+        <h3>Conheça os Novos Lançamentos</h3>
+        <div>
+          <strong>Gabriel Ribeiro</strong>
+          <p>1 min</p>
+          <p>15/04/21</p>
+        </div>
+      </article>
     </S.Container>
   )
 }
--- src/components/CardRecents/styles.ts
+++ src/components/CardRecents/styles.ts
@@ -2,26 +2,38 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 100%;
-  height: 10.2rem;
-  padding: 1.6rem;
-  border-radius: 16px;
+  height: auto;
 
-  border-left: 5px solid #37B6FE;
-
-  display: flex;
-  align-items: center;
-  flex-direction: column;
-  justify-content: center;
-
-  background: var(--background-card);
-
-  div{
-    width: 100%;
-    height: auto;
-    margin-top: 1rem;
+  article{
+    border-radius: 16px;
+    padding: 1.6rem 2.4rem;
+    border-left: 5px solid #37B6FE;
 
     display: flex;
     align-items: center;
-    justify-content: space-between;
+    flex-direction: column;
+    justify-content: center;
+
+    transition: 0.2s;
+    background: var(--background-card);
+
+    + article{
+      margin-top: 1.6rem;
+    }
+
+    :hover {
+      cursor: pointer;
+      transform: translateY(-0.35rem);
+    }
+
+    div{
+      width: 100%;
+      height: auto;
+      margin-top: 1rem;
+
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+    }
   }
 `;
